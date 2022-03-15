3c56185e60093582b2bde5a60c818d435947212b
guri-suhyun <myksh0903@naver.com>
2022-03-15 18:05:49 +0900

    0315 programmers 신고결과받기_추가

--- Programmers/Lv1_신고결과받기.py
+++ Programmers/Lv1_신고결과받기.py
@@ -14,5 +14,17 @@ def solution(id_list, report, k):
                     mail_user[id] += 1
     return list(mail_user.values())
 
+#-----다른 사람 풀이 참고하여 다시-----
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reports = {s:0 for s in id_list}
+    for i in set(report):
+        reports[i.split()[1]] += 1
+    for j in set(report):
+        if reports[j.split()[1]] >= k:
+            answer[id_list.index(j.split()[0])] += 1
+    return answer
+
+
 print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
 print(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
